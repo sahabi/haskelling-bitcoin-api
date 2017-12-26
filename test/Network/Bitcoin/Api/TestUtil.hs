c98738da6359727f71765612da134da319ff2259
sahabi <sahabi@gmail.com>
2017-12-25 23:17:35 -0600

    added rpc creds and fixed statuscodeexcetption

--- test/Network/Bitcoin/Api/TestUtil.hs
+++ test/Network/Bitcoin/Api/TestUtil.hs
@@ -2,14 +2,16 @@ module Network.Bitcoin.Api.TestUtil (testClient, isStatusCodeException) where
 
 import qualified Data.Text                                    as T (pack)
 import           Control.Lens                                 ((^.))
-import           Network.HTTP.Client                          (HttpException (..))
+import           Network.HTTP.Client                          ( HttpException (..)
+                                                              , HttpExceptionContent (..)
+                                                              , responseStatus )
 import           Network.Wreq.Lens                            (statusCode)
 
 import           Network.Bitcoin.Api.Client
 
 testClient :: (Client -> IO a) -> IO a
-testClient = withClient "127.0.0.1" 18332 (T.pack "user") (T.pack "pass")
+testClient = withClient "127.0.0.1" 8332 (T.pack "sahabi") (T.pack "5555")
 
-isStatusCodeException :: Int -> HttpException -> Bool
-isStatusCodeException code (StatusCodeException s _ _) = s ^. statusCode == code
+isStatusCodeException :: Int -> HttpExceptionContent -> Bool
+isStatusCodeException code (StatusCodeException s _ ) = (responseStatus s) ^. statusCode == code
 isStatusCodeException _ _ = False
